c0834c36e0c599cb782bc4b10130f2bf00351732
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-02-24 14:39:17 +0100

    Added database connection management with GORM

--- config/database.go
+++ config/database.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+
+	logLevel := logger.Info
+	if AppConfig.AppEnv == "production" {
+		logLevel = logger.Error
+	}
+
+	DB, err = gorm.Open(postgres.Open(AppConfig.DSN()), &gorm.Config{
+		Logger: logger.Default.LogMode(logLevel),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("failed to get sql db: %v", err)
+	}
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+
+	log.Println("Database connection established successfully")
+}
