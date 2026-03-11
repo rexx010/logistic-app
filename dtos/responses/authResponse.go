4c65e78bc0a92a8b877a96d536967a11d49396a3
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-03-11 22:58:34 +0100

    Add AuthResponse model for user authentication responses

---
+++ dtos/responses/authResponse.go
@@ -0,0 +1,8 @@
+package responses
+
+type AuthResponse struct {
+	AccessToken  string       `json:"access_token"`
+	RefreshToken string       `json:"refresh_token"`
+	ExpiresIn    int64        `json:"expires_in"`
+	User         UserResponse `json:"user"`
+}
