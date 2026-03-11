7201a42a345d1783cafbad1d1c6392c2970c0307
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-03-11 23:01:11 +0100

    Added `ProfilePicture` field to User model and commented out unused Notifications field

--- data/models/user.go
+++ data/models/user.go
@@ -19,14 +19,15 @@ const (
 
 type User struct {
 	Base
-	Name     string         `gorm:"not null" json:"name"`
-	Email    string         `gorm:"uniqueIndex;not null" json:"email"`
-	Phone    string         `gorm:"uniqueIndex" json:"phone"`
-	Password string         `gorm:"not null" json:"-"`
-	Role     RoleEnum       `gorm:"type:varchar(20);not null" json:"role"`
-	Status   UserStatusEnum `gorm:"type:varchar(20);default:'ACTIVE'" json:"status"`
+	Name           string         `gorm:"not null" json:"name"`
+	Email          string         `gorm:"uniqueIndex;not null" json:"email"`
+	Phone          string         `gorm:"uniqueIndex" json:"phone"`
+	Password       string         `gorm:"not null" json:"-"`
+	ProfilePicture string         `gorm:"default:null" json:"profile_picture"`
+	Role           RoleEnum       `gorm:"type:varchar(20);not null" json:"role"`
+	Status         UserStatusEnum `gorm:"type:varchar(20);default:'ACTIVE'" json:"status"`
 
 	BusinessProfile *BusinessProfile `gorm:"foreignKey:UserID" json:"businessProfile,omitempty"`
 	RiderProfile    *RiderProfile    `gorm:"foreignKey:UserID" json:"riderProfile,omitempty"`
-	Notifications   []Notification   `gorm:"foreignKey:UserID" json:"notifications,omitempty"`
+	//Notifications   []Notification   `gorm:"foreignKey:UserID" json:"notifications,omitempty"`
 }
