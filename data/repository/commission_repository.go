f47b487eb02fdba384e0621419e69384856c57cf
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-03-22 23:05:42 +0100

    Added `commission_repository.go` to define repository layer for commission data management

--- data/repository/commission_repository.go
+++ data/repository/commission_repository.go
@@ -1,1 +1,42 @@
 package repository
+
+import (
+	"context"
+	"errors"
+	"logisticApp/config"
+	"logisticApp/data/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type CommissionRepository interface {
+	Create(ctx context.Context, commission *models.Commission) error
+	FindByDeliveryID(ctx context.Context, deliveryID uuid.UUID) (*models.Commission, error)
+}
+
+type commissionRepository struct {
+	db *gorm.DB
+}
+
+func NewCommissionRepository() CommissionRepository {
+	return &commissionRepository{db: config.DB}
+}
+
+func (response *commissionRepository) Create(ctx context.Context, commission *models.Commission) error {
+	return response.db.WithContext(ctx).Create(commission).Error
+}
+
+func (response *commissionRepository) FindByDeliveryID(ctx context.Context, deliveryID uuid.UUID) (*models.Commission, error) {
+	var commission models.Commission
+	err := response.db.WithContext(ctx).
+		Where("delivery_id = ?", deliveryID).
+		First(&commission).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &commission, nil
+}
